2d8bab5eff5b7f5ced71f848f8bdc21790517e51
kagzouli <kagzouli@yahoo.fr>
2017-11-11 22:30:44 +0100

    KAR003 : Migration new version

--- BanqueService/src/main/java/com/banque/common/AppConfig.java
+++ BanqueService/src/main/java/com/banque/common/AppConfig.java
@@ -1,13 +1,13 @@
 package com.banque.common;
 
-import org.springframework.context.annotation.ComponentScans;
-
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cache.ehcache.EhCacheRegionFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.dialect.MySQL5Dialect;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,21 +18,9 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.banque.modele.EtatCompte;
-import com.banque.modele.OperationCompte;
-import com.banque.modele.Parametre;
-import com.banque.modele.Role;
-import com.banque.modele.User;
-
-
-
 @Configuration
 @EnableTransactionManagement
-@ComponentScans(value = { 
-	      @ComponentScan("com.banque.service"),
-	      @ComponentScan("com.banque.dao"),
-	      @ComponentScan("com.banque.common") 
-	    })
+@ComponentScan(basePackages="com.banque.service.* , com.banque.dao.*")
 public class AppConfig {
 	
 	/** Jndi banque name **/
@@ -56,15 +44,15 @@ public class AppConfig {
 		LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
 		localSessionFactoryBean.setDataSource(context.getBean(BANQUE_DATASOURCE_NAME, DataSource.class));
 		
-		localSessionFactoryBean.setAnnotatedClasses(Parametre.class, Role.class, User.class, EtatCompte. class , OperationCompte.class);
+		localSessionFactoryBean.setPackagesToScan("com.banque.modele");
 		
 		Properties props=new Properties();
-	    props.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-	    props.put("hibernate.cache.region.factory_class", EhCacheRegionFactory.class.getName());
-	    props.put("hibernate.cache.provider_configuration_file_resource_path", "ehcache.xml");
-	    props.put("hibernate.cache.use_query_cache", "true");
-	    props.put("hibernate.show_sql", "true");
-	    props.put("connection.autocommit", "false");
+	    props.put(AvailableSettings.DIALECT, MySQL5Dialect.class.getName());
+	    props.put(AvailableSettings.CACHE_REGION_FACTORY, EhCacheRegionFactory.class.getName());
+	    props.put(AvailableSettings.CACHE_PROVIDER_CONFIG, "ehcache.xml");
+	    props.put(AvailableSettings.USE_QUERY_CACHE, "true");
+	    props.put(AvailableSettings.SHOW_SQL, "true");
+	    props.put(AvailableSettings.AUTOCOMMIT, "false");
 	    localSessionFactoryBean.setHibernateProperties(props);
 	    
 	    return localSessionFactoryBean;
@@ -87,37 +75,4 @@ public class AppConfig {
 		return new SpringApplicationContext();
 	}
 	
-	/*
-
-	<tx:advice id="txAdviceTestFront" transaction-manager="txTestManager">
-		<tx:attributes>
-			<tx:method name="find*" propagation="REQUIRED" read-only="true" />
-			<tx:method name="*" propagation="REQUIRED"
-				rollback-for="com.banque.service.exception.BusinessException,com.banque.service.exception.TechnicalException" />
-		</tx:attributes>
-	</tx:advice>
-
-
-	<aop:config>
-		<aop:pointcut id="banqueServiceMethods"
-			expression="execution(* com.banque.service.impl.*BanqueImpl.*(..))" />
-		<aop:advisor advice-ref="txAdviceTestFront" pointcut-ref="banqueServiceMethods" />
-	</aop:config>
-
-	<aop:config>
-		<aop:pointcut id="banqueUserServiceMethods"
-			expression="execution(* com.banque.service.impl.*UserServiceImpl.*(..))" />
-		<aop:advisor advice-ref="txAdviceTestFront" pointcut-ref="banqueUserServiceMethods" />
-	</aop:config>
-
-	<!--  EH CACHE + SPRING -->
-	<bean id="cacheManager"
-		class="org.springframework.cache.ehcache.EhCacheManagerFactoryBean">
-		<property name="configLocation" value="classpath:ehcache.xml" />
-	</bean>
-
-	<bean id="autoproxy"
-		class="org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator" /> */
-
-
 }
